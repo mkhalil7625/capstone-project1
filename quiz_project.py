1617724e88ba24fa1e7037dc6888bfb1b245d7f3
Clara <10088152+claraj@users.noreply.github.com>
2021-09-13 17:00:05 -0500

    Update quiz_project.py

--- quiz_project.py
+++ quiz_project.py
@@ -9,64 +9,67 @@ If they answered all questions correctly, they will see another message
 letting them know they answered all the questions correctly.
 """
 
+# avoid creating functions inside other functions, unless the nested function is 
+# inherently tied to the containing function and doesn't make sense without it. It's fairly rare to create nested functions.
+def topic_entry(topics):
+    """
+    Docstring goes after the def line. 
+    User interface function to display list of topics and ask user for their choice, return the topic chosen
+    """
 
+  print('Enter topic from the following: ')
+    print(topics)
+    topic = input('Enter choice: ')
+    return topic.lower()
 
-
-
-
+    # validate topic is in the dictionary
+    while topic not in question_bank.keys():
+        print('please enter a valid input')
+        
+   
 def main():
  # dictionary to hold topics and their questions and answers
     question_bank = {
-        'space':{
-            'Which planet is closest to the sun?':'Mercury',
-            'Which planet spins in the opposite direction to all the others in the solar system?':'Venus',
-            'How many moons does Mars have?':'2',
-            'What was the first human-made object to leave the solar system?':'Voyager 1'
+        'space': {
+            'Which planet is closest to the sun?': 'Mercury',
+            'Which planet spins in the opposite direction to all the others in the solar system?': 'Venus',
+            'How many moons does Mars have?': '2',
+            'What was the first human-made object to leave the solar system?': 'Voyager 1'
         },
-        'art':{
-            'Who painted the Mona Lisa?':'Vincent Van Gogh',
-            'What precious stone is used to make the artist\'s pigment ultramarine?':'Lapiz lazuli',
-            'Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?':'Chicago'
+        'art': {
+            'Who painted the Mona Lisa?': 'Vincent Van Gogh',
+            'What precious stone is used to make the artist\'s pigment ultramarine?': 'Lapiz lazuli',
+            'Anish Kapoor\'s bean-shaped Cloud Gate scuplture is a landmark of which city?': 'Chicago'
         }
     }
-    """
-    function for user interface; the function asks the user for the topic
-    """
-    def topic_entry():
-        print('Enter topic from the following: ')
-        print(question_bank.keys())
-        topic = input('Enter choice: ')
-        return topic.lower()
 
-    topic=topic_entry()
+    topic = topic_entry(question_bank.keys())   # pass the list of topics to the function, handle validation in the function
 
-    #validate topic is in the dictionary
-    while topic not in question_bank.keys():
-        print('please enter a valid input')
-        topic=topic_entry()
-
-    #access the desired topic 
+    # access the desired topic 
     question_for_topic = question_bank[topic]
 
-    #get the total score after asking the questions
-    total_score=ask_questions(question_for_topic)
+    # get the total score after asking the questions
+    total_score = ask_questions(question_for_topic)
 
     print('End of quiz!')
     print(f'Your total score on {topic} questions is {total_score} out of {len(question_for_topic)}.')
 
-    if total_score == 3:
+    if total_score == 3:   # what if there are 2 or 3 or 100 questions? Will this work? Can you use len(questions_for_topic) in this condition? 
         print('You got all the answers correct!')
 
-"""
-the ask functions; it asks the user a series of questions related to the desired topic
-then check the answeres for correctness. the total score is calculated and returned
-"""
+
 def ask_questions(questions):
-    # todo needs to be split into more functions. 
+ 
+    """
+    the ask functions; it asks the user a series of questions related to the desired topic
+    then check the answeres for correctness. the total score is calculated and returned
+    """
+    
+   # todo needs to be split into more functions. (I think this is fine - if the quiz got more complex you'll probably need to break this down.)
     total_score = 0
     for q in questions:
         print(q)
-        answer = input('your answer is:')#get the answer
+        answer = input('your answer is:') # get the answer
 
         #validate the answer and check for correctness
         if answer.lower() ==  questions[q].lower():
@@ -75,7 +78,7 @@ def ask_questions(questions):
         else:
             print(f'Sorry, the answer is {questions[q]}')
 
-    return total_score #return score
+    return total_score #return score  This comment isn't needed. Comments should focus on explaining the complex parts of code. 
 
 main()
 
